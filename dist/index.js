427a2d0ed75b5430b20aa3610c21375e1e7bf50c
kylemocode <kylemo860617@gmail.com>
2021-02-21 21:15:38 +0800

    chore: remove unuse bundle file

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,2 @@
 
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./react-lite-yt-embed.cjs.production.min.js')
-} else {
-  module.exports = require('./react-lite-yt-embed.cjs.development.js')
-}
+module.exports = require('./react-lite-yt-embed.cjs.production.min.js')
